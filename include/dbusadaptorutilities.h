d5d9209b29ba4a9458cec5368cb0834e7e0b87db
Namratha-16 <98451191+Namratha-16@users.noreply.github.com>
2023-02-03 12:01:26 +0530

    Update dbusadaptorutilities.h

--- include/dbusadaptorutilities.h
+++ include/dbusadaptorutilities.h
@@ -1,1 +1,16 @@
 
+#pragma once
+
+#include <QString>
+#include <QVariant>
+#include <QDBusConnection>
+
+namespace DBusWrapper {
+
+void emitPropertiesChanged(const QString& path, const QString& interface, const QString& property, const QVariant& value);
+void emitPropertiesChanged(const QString& path, const QString& interface, const QVariantMap& changed_properties);
+
+void emitPropertiesChanged(const QDBusConnection& bus, const QString& path, const QString& interface, const QString& property, const QVariant& value);
+void emitPropertiesChanged(const QDBusConnection& bus, const QString& path, const QString& interface, const QVariantMap& changed_properties);
+
+} // namepsace DBusWrapper
